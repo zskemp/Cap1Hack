0669ac199857a731df2a7c411a6e4bb8d1b377d2
zskemp <zrskemp@gmail.com>
2017-06-07 16:25:33 -0400

    Fixed stupid little UI thingy

--- Visigoth/app/src/main/java/com/example/zrs/visigoth/TakeAuthVoiceActivity.java
+++ Visigoth/app/src/main/java/com/example/zrs/visigoth/TakeAuthVoiceActivity.java
@@ -1,13 +1,17 @@
 package com.example.zrs.visigoth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class TakeAuthVoiceActivity extends AppCompatActivity {
+
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,13 @@ public class TakeAuthVoiceActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        submit = (Button)findViewById(R.id.button);
+    }
+
+    public void dispatchToHome(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 
 }
